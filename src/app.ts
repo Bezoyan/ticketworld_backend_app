092492042ede9c651d435de67647b65eaa173fe4
bezoyan <zoya1905bezoyan@gmail.com>
2022-06-06 14:24:57 +0400

    Updated files

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,45 @@
 import { Application } from "express";
-import bodyParser from "body-parser";
+import bodyParser, { json } from "body-parser";
 import cors from "cors";
 import express from "express";
+import mongoose from "mongoose";
+import boom from 'express-boom';
+import 'dotenv/config'
+
+import { ticketRouters } from "./tickets/routes/";
+import { authRouters } from "./auth/routes";
+import { orderRouters } from "./orders/routes/index";
+import { eventRouters } from "./events/routes/index";
+import { createChargeRouter } from "./payments/routes/index";
 
 class App {
   public app: Application;
 
   constructor() {
     this.app = express();
+    this.app.use(boom());
     this.setConfig();
+    this.setMongoConfig();
+    this.app.set("trust proxy", true);
+    this.app.use(json());
   }
 
   private setConfig() {
-    // Allows us to receive requests with data in json format
     this.app.use(bodyParser.json({ limit: "50mb" }));
-    // Allows us to receive requests with data in x-www-form-urlencoded format
     this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-    // Enables cors
     this.app.use(cors());
+
+    // routes
+    this.app.use(ticketRouters);
+    this.app.use(authRouters);
+    this.app.use(orderRouters);
+    this.app.use(eventRouters);
+    this.app.use(createChargeRouter);
+  }
+
+  private setMongoConfig() {
+    mongoose.Promise = global.Promise;
+    mongoose.connect("mongodb://localhost:27017/ticketworld");
   }
 }
 
